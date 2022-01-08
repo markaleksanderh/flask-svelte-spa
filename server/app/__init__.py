514340cc085c7c26695b4b4ff4a8b5bf6a089c88
Mark Hil <mark@100shapes.com>
2022-01-08 17:54:31 +0000

    Working Svelte hello world

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import (
     Flask,
     jsonify,
+    send_from_directory,
 )
 
 from random import randint
-
 import os
+
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +14,11 @@ def create_app():
 
     @app.route('/')
     def index():
-        return jsonify({"message": "Hello, Flask Svelte SPA"})
+        return send_from_directory('client/public', 'index.html')
+    
+    @app.route("/<path:path>")
+    def home(path):
+        return send_from_directory('client/public', path)
 
     # Returns random integer between 0 and 100
     @app.route('/randint')
